daee746f3832607bbba2af8b6761795ef8c81859
Taffie Theuri <taffietheuri4@gmail.com>
2022-11-23 15:13:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let multiply;
+const num1 = 31;
+const num2= 2;
+
+ multiply = num1*num2;
+
+
+ let random = Math.random()+1;
+ random = Math.floor(random);
+
+
+ const num3 = 22;
+ const num4=6;
+
+ let mod = num3%num4;
+
+ const max = (2,3,4,6,8,12,16,20);
+
+Math.max (max);
